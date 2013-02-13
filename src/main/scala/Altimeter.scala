83fc29582842b13be3db52585d1e2b078f112da6
Dan Luu <danluu@gmail.com>
2013-02-13 15:30:57 -0500

    rename, add scaffolding

---
+++ src/main/scala/Altimeter.scala
@@ -0,0 +1,11 @@
+package zzz.akka.avionics
+
+import akka.actor.{Props, Actor, ActorSystem, ActorLogging}
+import scala.concurrent.util.duration._
+//  The  Scheduler  needs  an  execution  context  -  we'll  just  use  the  global  one
+import  scala.concurrent.ExecutionContext.Implicits.global
+
+object Altimeter{
+
+
+}
