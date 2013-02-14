ba7490fef32468b0723fdc9174b18cb041be00c9
Dan Luu <danluu@gmail.com>
2013-02-14 12:34:26 -0500

    EventSource

---
+++ src/main/scala/EventSource.scala
@@ -0,0 +1,21 @@
+package zzz.akka.avionics
+
+import akka.actor.{Actor, ActorRef}
+import java.util
+
+object EventSource{
+  case class RegisterListener(listener: ActorRef)
+  case class UnregisterListener(listener: ActorRef)
+}
+
+trait EventSource { this: Actor =>
+  import EventSource._
+
+  var listeners = scala.collection.immutable.Vector.empty[ActorRef]
+  def sendEvent[T](event: T): Unit = listeners.foreach{ _ ! event}
+
+  def eventSourceReceive: Receive = {
+    case RegisterListener(listener)   => listeners = listeners :+ listener
+    case UnregisterListener(listener) => listeners = listeners.filter{_ != listener}
+  }
+}
