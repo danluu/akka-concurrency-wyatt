6ddead76db5d95401218fb1ad93b0a27bee82d74
Dan Luu <danluu@gmail.com>
2013-02-19 15:35:00 -0500

    add FlightAttendant class

---
+++ src/main/scala/FlightAttendant.scala
@@ -0,0 +1,27 @@
+package zzz.akka.avionics
+
+import akka.actor.Actor
+import scala.concurrent.duration._
+
+trait AttendantResponsiveness { 
+  val maxResponseTimeMS: Int
+  def responseDuration = scala.util.Random.nextInt(maxResponseTimeMS).millis
+}
+
+object FlightAttendant { 
+  case class GetDrink(drinkname: String)
+  case class Drink(drinkname: String)
+
+  def apply() = new FlightAttendant with 
+  AttendantResponsiveness { val maxResponseTimeMS = 300000 } //5 min, by default
+}
+
+class FlightAttendant extends Actor { this: AttendantResponsiveness => 
+  import FlightAttendant._
+  
+  implicit val ec = context.dispatcher
+  def receive = { 
+    case GetDrink(drinkname) =>
+      context.system.scheduler.scheduleOnce(responseDuration, sender, Drink(drinkname))
+  }				     
+}
