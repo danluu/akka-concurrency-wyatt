88cec6eeb545995a6a0b39cbbdf6b1bcbf04062b
Dan Luu <danluu@gmail.com>
2013-02-19 18:05:16 -0500

    add CoPilot

--- src/main/scala/Pilot.scala
+++ src/main/scala/Pilot.scala
@@ -31,3 +31,17 @@ class Pilot extends Actor{
       controls = context.system.deadLetters
   }
 }
+
+class CoPilot extends Actor { 
+  import Pilots._
+  var controls: ActorRef = context.system.deadLetters
+  var pilot: ActorRef = context.system.deadLetters
+  var autopilot: ActorRef = context.system.deadLetters
+  val pilotName = context.system.settings.config.getString(
+  "zzz.akka.avionics.flightcrew.pilotName")
+  def receive = { 
+    case ReadyToGo =>
+      pilot = context.actorFor("../" + pilotName)
+      autopilot = context.actorFor("../AutoPilot")
+  }
+}
