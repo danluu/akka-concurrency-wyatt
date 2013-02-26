cc895fd7bbfb7b3b48362bce6c56e82bdf9707c6
Dan Luu <danluu@gmail.com>
2013-02-26 13:02:40 -0500

    PassengerSupervisor

---
+++ src/main/scala/PassengerSupervisor.scala
@@ -0,0 +1,70 @@
+package zzz.akka.avionics
+
+import akka.actor.{ActorRef, Actor, Props, ActorKilledException, ActorInitializationException}
+import akka.actor.SupervisorStrategy._
+import akka.actor.{OneForOneStrategy}
+
+object PassengerSupervisor {
+  case object GetPassengerBroadcaster
+  case class PassengerBroadcaster(broadcaster: ActorRef)
+  def apply(callButton: ActorRef) = new PassengerSupervisor(callButton) with PassengerProvider
+}
+
+class PassengerSupervisor(callButton: ActorRef) extends Actor { this: PassengerProvider =>
+  import PassengerSupervisor._
+  override val supervisorStrategy = OneForOneStrategy() {
+    case _: ActorKilledException => Escalate
+    case _: ActorInitializationException => Escalate
+    case _ => Resume
+  }
+
+  //internal messages
+  case class GetChildren(forSomeone: ActorRef)
+  case class Children(children: Iterable[ActorRef], childrenFor: ActorRef)
+
+  override def preStart() {
+    context.actorOf(Props(new Actor {
+      val config = context.system.settings.config
+      override val supervisorStrategy = OneForOneStrategy() {
+        case _: ActorKilledException => Escalate
+        case _: ActorInitializationException => Escalate
+        case _ => Stop
+      }
+      override def preStart() {
+        import scala.collection.JavaConverters._
+        import com.typesafe.config.ConfigList
+        val passengers = config.getList("zzz.akka.avionics.passengers")
+        passengers.asScala.foreach { nameWithSeat =>
+          val id = nameWithSeat.asInstanceOf[ConfigList]
+            .unwrapped().asScala.mkString("-")
+            .replaceAllLiterally(" ", "_")
+          context.actorOf(Props(newPassenger(callButton)), id)
+        }
+      }
+      def receive = {
+        case GetChildren(forSomeone: ActorRef) =>
+          sender ! Children(context.children, forSomeone)
+      }
+    }), "PassengersSupervisor")
+  }
+
+  def noRouter: Receive = {
+    case GetPassengerBroadcaster =>
+      val passengers = context.actorFor("PassengersSupervisor")
+      passengers ! GetChildren(sender)
+    case Children(passengers, destinedFor) =>
+      val router = context.actorOf(Props().withRouter(
+        BroadcastRouter(passengers.toSeq)), "Passengers")
+      destinedFor ! PassengerBroadcaster(router)
+      context.become(withRouter(router))
+  }
+
+  def withRouter(router: ActorRef): Receive = {
+    case GetPassengerBroadcaster =>
+      sender ! PassengerBroadcaster(router)
+    case Children(_, destinedFor) =>
+      destinedFor ! PassengerBroadcaster(router)
+  }
+
+  def receive = noRouter
+}
